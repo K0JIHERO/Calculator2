02337d91dd8a0e9babb366bc2d306d28b9286a5c
K0JIHERO <guagetru.bla@gmail.com>
2020-10-28 21:46:25 +0900

    Initial commit

---
+++ src/Hybrid.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Hybrid {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Input: ");
+        String userResponse = s.nextLine();
+        System.out.println(produceAnswer(userResponse));
+    }
+
+    public static int produceAnswer(String input) {
+        Scanner s = new Scanner(input);
+        String operand1 = s.next();
+        String operator = s.next();
+        String operand2 = s.next();
+        int num1 = Integer.parseInt(operand1);
+        int num2 = Integer.parseInt(operand2);
+        int answer = 0;
+        if (num1 >= 1 && num1 <= 10 && num2 >= 1 && num2 <= 10) {
+            if (operator.equals("+")) {
+                answer = num1 + num2;
+            } else if (operator.equals("-")) {
+                answer = num1 - num2;
+            } else if (operator.equals("/")) {
+                answer = num1 / num2;
+            } else if (operator.equals("*")) {
+                answer = num1 * num2;
+            } else {
+                System.out.println("Задан неверный параметр!");
+            }
+        }
+
+        else {
+            System.out.println("Задано неверное число!");
+        }
+        System.out.println("Output: ");
+        return answer;
+
+    }
+}
+
